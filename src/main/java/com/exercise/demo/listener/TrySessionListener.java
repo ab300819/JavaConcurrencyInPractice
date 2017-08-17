aa8fe6091a634fd37b46bb768caa4d7bfe8d9ca5
Mason <bigger831>
2017-08-17 16:11:29 +0800

    add Filter and Listener

---
+++ src/main/java/com/exercise/demo/listener/TrySessionListener.java
@@ -0,0 +1,22 @@
+package com.exercise.demo.listener;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Created by lenovo on 2017/8/17.
+ */
+@WebListener
+public class TrySessionListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        System.out.println("SessionListener创建");
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        System.out.println("SessionListener销毁");
+
+    }
+}
