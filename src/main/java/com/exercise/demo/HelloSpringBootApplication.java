92d289fca33ef678105d99b6ba8bcd8a225895ff
Mason <bigger831>
2017-10-14 18:22:01 +0800

    save

--- src/main/java/com/exercise/demo/HelloSpringBootApplication.java
+++ src/main/java/com/exercise/demo/HelloSpringBootApplication.java
@@ -1,5 +1,9 @@
 package com.exercise.demo;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
@@ -14,6 +18,24 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 //@ServletComponentScan
 public class HelloSpringBootApplication {
+
+    final static String queueName = "spring-boot";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange(){
+        return new TopicExchange("spring-boot-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue,TopicExchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with(queueName)
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringBootApplication.class, args);
 
