0b3a6d46517e990e1df30f4c2b386cb0fb2b7f08
mason <mengshen@dxy.cn>
2018-12-21 20:23:25 +0800

    更新

--- src/main/java/com/exercise/demo/configuration/WebMvcConfiguration.java
+++ src/main/java/com/exercise/demo/configuration/WebMvcConfiguration.java
@@ -4,12 +4,12 @@ import com.exercise.demo.parameter.ArgumentParameterResolve;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private ArgumentParameterResolve argumentParameterResolve;
 
@@ -20,7 +20,6 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        super.addArgumentResolvers(argumentResolvers);
         argumentResolvers.add(argumentParameterResolve);
     }
 }
