2d3c8650dcd39deb2dbe8c76198ba89b9dd5fa71
mason <mengshen@dxy.cn>
2018-12-21 17:10:28 +0800

    添加定时任务

--- src/main/java/com/exercise/demo/Application.java
+++ src/main/java/com/exercise/demo/Application.java
@@ -3,6 +3,7 @@ package com.exercise.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Hello world!
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.exercise.demo.mybatis.dao"})
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/exercise/demo/schedule/Schedule.java
@@ -0,0 +1,35 @@
+package com.exercise.demo.schedule;
+
+import com.exercise.demo.common.controller.ProcessController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+public class Schedule {
+
+    private static Logger logger = LoggerFactory.getLogger(ProcessController.class);
+
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void outputEveryFiveSecond() {
+
+        logger.debug("计数：{}", nowTime(new Date()));
+    }
+
+    private String nowTime(Date date) {
+
+        LocalTime localTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalTime();
+        String hour = localTime.getHour() < 10 ? "0" + localTime.getHour() : String.valueOf(localTime.getHour());
+        String minute = localTime.getMinute() < 10 ? "0" + localTime.getMinute() : String.valueOf(localTime.getMinute());
+        String second = localTime.getSecond() < 10 ? "0" + localTime.getSecond() : String.valueOf(localTime.getSecond());
+
+        return hour + ":" + minute + ":" + second;
+
+    }
+
+}
