a333fcd96a633911599d8a084075e80b3f8f8be2
Mason <ab300819@163.com>
2019-03-11 00:18:48 +0800

    添加跨域支持

--- src/main/java/com/exercise/demo/configuration/MyWebMvcConfigurer.java
+++ src/main/java/com/exercise/demo/configuration/MyWebMvcConfigurer.java
@@ -4,22 +4,40 @@ import com.exercise.demo.parameter.ArgumentParameterResolve;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class WebMvcConfiguration implements WebMvcConfigurer {
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
     private ArgumentParameterResolve argumentParameterResolve;
 
     @Autowired
-    public WebMvcConfiguration(ArgumentParameterResolve argumentParameterResolve) {
+    public MyWebMvcConfigurer(ArgumentParameterResolve argumentParameterResolve) {
         this.argumentParameterResolve = argumentParameterResolve;
     }
 
+    /**
+     * 处理自定义注解参数处理
+     *
+     * @param argumentResolvers
+     */
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(argumentParameterResolve);
+    }
+
+    /**
+     * 处理跨域
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**");
+
     }
 }
