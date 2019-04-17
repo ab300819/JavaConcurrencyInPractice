5653b01c9d661b220d7a9de8d37ead01303a6c17
Mason <ab300819@163.com>
2019-04-18 01:07:57 +0800

    生成数据库dto，dao，entity

--- src/main/java/com/exercise/demo/mybatis/dao/CountryDao.java
+++ src/main/java/com/exercise/demo/mybatis/dao/CountryDao.java
@@ -2,17 +2,12 @@ package com.exercise.demo.mybatis.dao;
 
 import com.exercise.demo.mybatis.dto.CountryDto;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.SelectProvider;
 
 
 /**
- * Project: ExerciseTimer
- * Package: com.exercise.database.dao
- * Author: mason
- * Time: 11:33
- * Date: 2018-03-29
- * Created with IntelliJ IDEA
+ * @author mason
+ * @date 2019-03-29
  */
 @Mapper
 public interface CountryDao {
