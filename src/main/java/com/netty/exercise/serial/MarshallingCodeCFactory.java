fcceee21eb672d6fcd73fbe844adcd98dc240516
Mason <ab300819@163.com>
2019-07-16 01:13:50 +0800

    使用 Marshalling 编解码包

---
+++ src/main/java/com/netty/exercise/serial/MarshallingCodeCFactory.java
@@ -0,0 +1,17 @@
+package com.netty.exercise.serial;
+
+import io.netty.handler.codec.marshalling.MarshallingDecoder;
+
+public final class MarshallingCodeCFactory {
+
+    public static MarshallingDecoder buildMarshallingDecoder(){
+
+        return null;
+    }
+
+    public static MarshallingDecoder buildMarshallingEncoder(){
+
+        return null;
+    }
+
+}
