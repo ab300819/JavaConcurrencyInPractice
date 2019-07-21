9e034146bbb19f455edd5e3e476ab6b52d731feb
mason <ab300819@163.com>
2019-07-22 01:09:03 +0800

    解析 http 协议

---
+++ src/main/java/com/netty/exercise/http/HttpFileServer.java
@@ -0,0 +1,70 @@
+package com.netty.exercise.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class HttpFileServer {
+
+    private static final String DEFAULT_URL = "/src/main/java/com/netty/exercise/http/";
+
+    public static void main(String[] args) throws Exception {
+        new HttpFileServer().start(DEFAULT_URL, 9090);
+    }
+
+    public void start(String url, int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        ServerBootstrap b = new ServerBootstrap();
+
+        try {
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel ch) {
+                            ch.pipeline().addLast(new HttpRequestDecoder());
+                            ch.pipeline().addLast(new HttpObjectAggregator(65536));
+                            ch.pipeline().addLast(new HttpResponseEncoder());
+                            ch.pipeline().addLast(new ChunkedWriteHandler());
+                            ch.pipeline().addLast(new HttpFileServerHandler(url));
+                        }
+                    });
+            ChannelFuture f = b.bind("127.0.0.1", port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    public static class HttpFileServerHandler extends ChannelInboundHandlerAdapter {
+
+        private static final Logger log = LoggerFactory.getLogger(HttpFileServerHandler.class);
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+            List<Object> test = (List<Object>) msg;
+            log.debug("Receive client:[{}]", msg);
+            ctx.writeAndFlush("has receive");
+
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+            ctx.close();
+            cause.printStackTrace();
+        }
+    }
+
+}
