b52645012498ee06d34b73ffae85158605d0650a
mason <ab300819@163.com>
2019-08-01 01:31:10 +0800

    测试序列化性能

--- src/main/java/com/netty/exercise/serial/TestSerial.java
+++ src/main/java/com/netty/exercise/serial/TestSerial.java
@@ -2,8 +2,13 @@ package com.netty.exercise.serial;
 
 import org.msgpack.annotation.Message;
 
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
 @Message
-public class TestSerial {
+public class TestSerial implements Serializable {
+
+    private static final long serialVersionUID = 6426785127985794642L;
 
     private int age;
 
@@ -23,5 +28,31 @@ public class TestSerial {
 
     public void setTest(String test) {
         this.test = test;
+    }
+
+    public byte[] codeC() {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        byte[] value = this.test.getBytes();
+        buffer.putInt(value.length);
+        buffer.put(value);
+        buffer.putInt(this.age);
+        buffer.flip();
+        value = null;
+        byte[] result = new byte[buffer.remaining()];
+        buffer.get(result);
+        return result;
+    }
+
+    public byte[] codeC(ByteBuffer buffer) {
+        buffer.clear();
+        byte[] value = this.test.getBytes();
+        buffer.putInt(value.length);
+        buffer.put(value);
+        buffer.putInt(this.age);
+        buffer.flip();
+        value = null;
+        byte[] result = new byte[buffer.remaining()];
+        buffer.get(result);
+        return result;
     }
 }
