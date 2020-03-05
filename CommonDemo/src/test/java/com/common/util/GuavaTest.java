0aacc2dec6c3617b374b0fd2e477c940b4621d8d
mason <ab300819@163.com>
2020-03-05 23:53:25 +0800

    添加 dubbo

---
+++ CommonDemo/src/test/java/com/common/util/GuavaTest.java
@@ -0,0 +1,13 @@
+package com.common.util;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Predicates;
+import org.junit.Test;
+
+public class GuavaTest {
+
+    @Test
+    public void ObjectsTest(){
+    }
+
+}
---
+++ DubboDemo/server-api/src/main/java/org/dubbo/api/DemoService.java
@@ -0,0 +1,5 @@
+package org.dubbo.api;
+
+public interface DemoService {
+    String sayHello();
+}
