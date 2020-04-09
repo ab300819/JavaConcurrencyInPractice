485c2d8bdc7326227612a18b22e14509b9ceae8e
mason <ab300819@163.com>
2020-04-09 18:41:32 +0800

    fix: 修复服务端解码错误

--- NettyDemo/src/main/java/com/geektime/demo/server/codec/OrderProtocolDecoder.java
+++ NettyDemo/src/main/java/com/geektime/demo/server/codec/OrderProtocolDecoder.java
@@ -1,6 +1,6 @@
 package com.geektime.demo.server.codec;
 
-import com.geektime.demo.common.ResponseMessage;
+import com.geektime.demo.common.RequestMessage;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
@@ -10,10 +10,10 @@ import java.util.List;
 public class OrderProtocolDecoder extends MessageToMessageDecoder<ByteBuf> {
 
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
-        ResponseMessage responseMessage = new ResponseMessage();
-        responseMessage.decode(msg);
-        out.add(responseMessage);
+    protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) {
+        RequestMessage requestMessage = new RequestMessage();
+        requestMessage.decode(msg);
+        out.add(requestMessage);
     }
 
 }
