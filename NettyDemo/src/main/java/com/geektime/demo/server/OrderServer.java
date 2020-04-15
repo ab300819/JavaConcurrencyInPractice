5bcde738993f8b5c7d39782b821955b6dec5d567
mason <ab300819@163.com>
2020-04-15 20:48:59 +0800

    perf: 添加监控

--- NettyDemo/src/main/java/com/geektime/demo/server/OrderServer.java
+++ NettyDemo/src/main/java/com/geektime/demo/server/OrderServer.java
@@ -8,13 +8,16 @@ import com.geektime.demo.server.codec.OrderFrameDecoder;
 import com.geektime.demo.server.codec.OrderFrameEncoder;
 import com.geektime.demo.server.codec.OrderProtocolDecoder;
 import com.geektime.demo.server.codec.OrderProtocolEncoder;
+import com.geektime.demo.server.handler.MetricHandler;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioChannelOption;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
 
 /**
  * Order 服务端
@@ -29,23 +32,30 @@ public class OrderServer {
 
     public static void start(int port) throws InterruptedException {
 
-        EventLoopGroup boss = new NioEventLoopGroup();
-        EventLoopGroup work = new NioEventLoopGroup();
+        EventLoopGroup boss = new NioEventLoopGroup(0, new DefaultThreadFactory("boss"));
+        EventLoopGroup work = new NioEventLoopGroup(0, new DefaultThreadFactory("work"));
         ServerBootstrap serverBootstrap = new ServerBootstrap();
 
         serverBootstrap.channel(NioServerSocketChannel.class);
         serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
         serverBootstrap.group(boss, work);
+        serverBootstrap.childOption(NioChannelOption.TCP_NODELAY, true);
+        serverBootstrap.option(NioChannelOption.SO_BACKLOG, 1024);
 
         serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
             protected void initChannel(NioSocketChannel ch) {
                 ChannelPipeline pipeline = ch.pipeline();
-                pipeline.addLast(new OrderFrameDecoder());
-                pipeline.addLast(new OrderFrameEncoder());
-                pipeline.addLast(new OrderProtocolEncoder());
-                pipeline.addLast(new OrderProtocolDecoder());
-                pipeline.addLast(new OrderServerHandler());
+                pipeline.addLast(new LoggingHandler(LogLevel.DEBUG));
+
+                pipeline.addLast("frameDecoder", new OrderFrameDecoder());
+                pipeline.addLast("frameEncoder", new OrderFrameEncoder());
+                pipeline.addLast("protocolEncoder", new OrderProtocolEncoder());
+                pipeline.addLast("protocolDecoder", new OrderProtocolDecoder());
+                pipeline.addLast("metrics", new MetricHandler());
+
+                pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+                pipeline.addLast("serverHandler", new OrderServerHandler());
             }
         });
 
