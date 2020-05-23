25951615fb098acecb29692e9daaca81721c4122
mason <ab300819@163.com>
2020-05-23 22:31:08 +0800

    feat: use xml to cofigure bean

---
+++ spring/src/main/java/org/spring/explore/dependency/injection/UserHolder.java
@@ -0,0 +1,14 @@
+package org.spring.explore.dependency.injection;
+
+import lombok.Data;
+import org.spring.explore.common.domain.User;
+
+/**
+ * @author mason
+ */
+@Data
+public class UserHolder {
+
+    private User user;
+
+}
