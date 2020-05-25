a0a2c16f7a56326b28046cfcb9312a1d8f8e532e
mason <ab300819@163.com>
2020-05-25 15:24:12 +0800

    refactor: move code to test

--- spring/src/main/java/org/spring/explore/dependency/lookup/ObjectProviderDemo.java
+++ spring/src/main/java/org/spring/explore/dependency/lookup/ObjectProviderDemo.java
@@ -1,10 +1,8 @@
 package org.spring.explore.dependency.lookup;
 
 import lombok.extern.slf4j.Slf4j;
-import org.spring.explore.common.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
@@ -15,21 +13,6 @@ import org.springframework.context.annotation.Primary;
  */
 @Slf4j
 public class ObjectProviderDemo {
-
-    public static void main(String[] args) {
-
-        // 创建 BeanFactory 容器
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(ObjectProviderDemo.class);
-        // 启动应用上下文
-        applicationContext.refresh();
-        // 依赖查找集合对象
-        lookupByObjectProvider(applicationContext);
-        lookupIfAvailable(applicationContext);
-        lookupByStreamOps(applicationContext);
-
-        applicationContext.close();
-    }
 
     @Bean
     @Primary
@@ -45,22 +28,5 @@ public class ObjectProviderDemo {
     public static void showBean(ApplicationContext applicationContext) {
         ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
         log.info(objectProvider.getObject());
-    }
-
-    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
-        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
-        User user = userObjectProvider.getIfAvailable(User::createUser);
-        log.info("当前对象：{}", user.toString());
-    }
-
-    private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
-        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
-        log.info(objectProvider.getObject());
-    }
-
-
-    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
-        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
-        objectProvider.stream().forEach(log::info);
     }
 }
