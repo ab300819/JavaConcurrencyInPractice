691c16c524a32eba803dddde01481e6fa88df48f
Mason <ab300819@163.com>
2020-07-22 22:34:17 +0800

    feat: add request scope

--- spring-mvc/src/main/java/com/demo/controller/AppController.java
+++ spring-mvc/src/main/java/com/demo/controller/AppController.java
@@ -1,12 +1,13 @@
 package com.demo.controller;
 
+import com.demo.entity.User;
 import com.demo.event.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 测试用 contoller
+ * 测试用 controller
  *
  * @author mason
  */
@@ -16,10 +17,13 @@ public class AppController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private User user;
+
     @GetMapping("/event")
-    public String userRegisterEvent() {
+    public User userRegisterEvent() {
         userService.register("mason");
-        return "Hello world";
+        return user;
     }
 
 }
