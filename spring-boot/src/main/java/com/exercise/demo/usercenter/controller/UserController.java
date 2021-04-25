321aa8cd375c6546d85320e31e18738f0a3b5511
Mason <ab300819@163.com>
2021-04-25 23:59:56 +0800

    feat: 测试 mybatis 注入 dao

--- spring-boot/src/main/java/com/exercise/demo/usercenter/controller/UserController.java
+++ spring-boot/src/main/java/com/exercise/demo/usercenter/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.exercise.demo.usercenter.controller;
 
 import com.exercise.demo.common.dto.ReturnResult;
+import com.exercise.demo.mybatis.dao.ImUserDao;
+import com.exercise.demo.mybatis.dto.ImUserDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private ImUserDao imUserDao;
+
     @GetMapping("/login")
     public ReturnResult login(String userName, String password) {
         log.info("hello");
+        ImUserDto imUser = imUserDao.selectByPrimaryKey(1);
+        log.info(imUser.toString());
         return null;
     }
 }
