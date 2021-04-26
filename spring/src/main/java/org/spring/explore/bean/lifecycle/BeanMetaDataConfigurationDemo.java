d6b117353adf6fc1fc6e2e2b139859f3f929f8eb
Mason <ab300819@163.com>
2021-04-27 00:27:04 +0800

    feat: use properties to define bean

---
+++ spring/src/main/java/org/spring/explore/bean/lifecycle/BeanMetaDataConfigurationDemo.java
@@ -0,0 +1,28 @@
+package org.spring.explore.bean.lifecycle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.spring.explore.common.domain.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.EncodedResource;
+
+@Slf4j
+public class BeanMetaDataConfigurationDemo {
+
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        PropertiesBeanDefinitionReader reader = new PropertiesBeanDefinitionReader(beanFactory);
+
+        String location = "META-INF/user.properties";
+        Resource resource = new ClassPathResource(location);
+        EncodedResource encodedResource = new EncodedResource(resource, "UTF-8");
+        int beanCount = reader.loadBeanDefinitions(encodedResource);
+        log.info("{} beans has been load.", beanCount);
+
+        User user = (User) beanFactory.getBean("test");
+        log.info(user.toString());
+    }
+
+}
