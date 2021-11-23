e1259f696eb6c036c7fba8f30f8bb9082cccfe99
Mason <ab300819@163.com>
2021-11-23 21:02:00 +0800

    feat: 添加 netty 模拟服务

---
+++ netty/src/main/java/org/explorer/demo/NettyRedisServe.java
@@ -0,0 +1,50 @@
+package org.explorer.demo;
+
+import java.nio.charset.StandardCharsets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * <p>模拟 redis 协议</p>
+ *
+ * @author mengshen
+ * @date 2021/11/23 20:24
+ */
+public class NettyRedisServe {
+
+    private static final Logger log = LoggerFactory.getLogger(NettyRedisServe.class);
+
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        serverBootstrap.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+                                log.info(msg);
+                                ctx.writeAndFlush("+ok\r\n".getBytes(StandardCharsets.UTF_8));
+                            }
+                        });
+                    }
+                }).bind(8090);
+    }
+
+}
